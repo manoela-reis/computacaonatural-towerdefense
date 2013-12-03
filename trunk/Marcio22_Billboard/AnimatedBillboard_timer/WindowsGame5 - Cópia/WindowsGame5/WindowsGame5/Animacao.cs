755e6553e12f6a5f8460dad20eb453ed94db2429
marciomiranda.nave@gmail.com <marciomiranda.nave@gmail.com@5fd0e342-e26b-3673-cab7-61a43f8e4dae>
2013-12-03 20:45:00 +0000

    Versão do billboard com animação em 1 segundo

---
+++ trunk/Marcio22_Billboard/AnimatedBillboard_timer/WindowsGame5 - Cópia/WindowsGame5/WindowsGame5/Animacao.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace WindowsGame5
+{
+    class Animacao
+    {
+
+        public Texture2D texture;
+        public Texture2D[] textures = new Texture2D[2];
+        private int id;
+        private float cronometro;
+        private float tempo;
+
+        public Animacao(float tempo, int qntTexturas)
+        {
+            this.tempo = tempo;
+            textures = new Texture2D[qntTexturas];
+        }
+
+        public void Update(GameTime e){
+            cronometro += (float)e.ElapsedGameTime.Milliseconds / 1000;
+            
+            if(cronometro >= tempo)
+            {
+                cronometro = 0;                
+                id++;
+                if (id > textures.Length - 1)
+                {
+                    id = 0;
+                }
+                texture = textures[id];
+            }
+        }
+    }
+}
---
+++ trunk/Marcio22_Billboard/AnimatedBillboard_timer/WindowsGame5 - Cópia/WindowsGame5/WindowsGame5/Arvore.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace WindowsGame5
+{
+    class Arvore
+    {
+        Texture2D textureMoinho;
+        public VertexPositionTexture[] verts;
+        VertexBuffer vertexBuffer;
+        public Matrix world;
+        public Vector3 position;
+
+        public Arvore(Vector3 position, Vector3 positionCamera, GraphicsDevice graphicsDevice)
+        {
+            world = Matrix.CreateConstrainedBillboard(position, positionCamera, Vector3.Up, Vector3.Forward, Vector3.Forward);
+
+            this.position = position;
+
+            verts = new VertexPositionTexture[4];
+            verts[0] = new VertexPositionTexture(new Vector3(-1, 3, 0), new Vector2(0, 0));
+            verts[1] = new VertexPositionTexture(new Vector3(1, 3, 0), new Vector2(1, 0));
+            verts[2] = new VertexPositionTexture(new Vector3(-1, -1, 0), new Vector2(0, 1));
+            verts[3] = new VertexPositionTexture(new Vector3(1, -1, 0), new Vector2(1, 1));
+
+            vertexBuffer = new VertexBuffer(graphicsDevice, typeof(VertexPositionTexture),
+                verts.Length, BufferUsage.None);
+            vertexBuffer.SetData<VertexPositionTexture>(verts);
+        }
+
+        public void Update(Vector3 positionCamera)
+        {
+            world = Matrix.CreateConstrainedBillboard(position, positionCamera, Vector3.Up, Vector3.Forward, Vector3.Forward);
+
+        }      
+    }
+}
